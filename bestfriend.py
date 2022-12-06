5b0f123b82e294df66e3a245f6c36acc5af64215
Brendon Wright <brendonewright@gmail.com>
2022-12-06 14:01:19 -0500

    First weird commit

--- bestfriend.py
+++ bestfriend.py
@@ -1,2 +1,10 @@
+
 first_name= input("What is your name? ")
+is_it_really= input("Are you sure? ")
+like_really= input("Like absoluely? ")
+print("Great! but just to check...")
+typos= input("No typos right? ")
+confirm= input(f"Just to be sure you would like to use the name {first_name}, correct? ")
+no_change= input("It can not be changed later would you still like ot proceed? ")
+
 print(f"Hello, {first_name}")
